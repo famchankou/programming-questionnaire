bacb8304b2eccd1cf1ee8253e4dc3a9a5af53dde
Vadzim Sheukunou <vadzim.sheukunou@itechart-group.com>
2020-02-22 18:59:23 +0300

    feat: added controllers for get questioniries

--- programming-questionnaire-server/src/controllers/questionnaire.controller.js
+++ programming-questionnaire-server/src/controllers/questionnaire.controller.js
@@ -15,10 +15,40 @@ export default class QuestionnaireController {
   }
 
   static async get(req, res) {
-    res.json('');
+    try {
+      const questionary = await models.Questionnaire.findByPk(
+        `${req.params.questId}` || '',
+      );
+
+      if (questionary) {
+        res.status(200).send(questionary);
+      } else {
+        res.status(404).send({
+          message: 'Questionary Not Found',
+        });
+      }
+    } catch (error) {
+      res.status(400).send({
+        message: `Invalid Questionary ID: ${error.message}`,
+      });
+    }
   }
 
   static async getAll(req, res) {
-    res.json('');
+    try {
+      const questionaries = await models.Questionnaire.findAll();
+
+      if (questionaries) {
+        res.status(200).send(questionaries);
+      } else {
+        res.status(404).send({
+          message: 'Questionaries Not Found',
+        });
+      }
+    } catch (error) {
+      res.status(400).send({
+        message: `${error.message}`,
+      });
+    }
   }
 }
