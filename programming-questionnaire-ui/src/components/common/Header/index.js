655b4032c2db5928a90d0344379053d4cdc15a6c
Vadzim Sheukunou <vadzim.sheukunou@itechart-group.com>
2020-02-22 19:00:57 +0300

    feat: added header component and page template

---
+++ programming-questionnaire-ui/src/components/common/Header/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { unauthorize } from 'store/auth/login';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import withStyles from '@material-ui/core/styles/withStyles';
+import styles from './styles';
+
+const HeaderComponent = ({ classes }) => {
+  const username = useSelector(state => state.auth.user.username);
+  const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    dispatch(unauthorize());
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Programming quizes
+          </Typography>
+          <Typography>{username}</Typography>
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default withStyles(styles)(HeaderComponent);
