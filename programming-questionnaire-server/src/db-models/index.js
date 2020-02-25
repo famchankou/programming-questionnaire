67cc849a12e931e619aef6684aae97286eabae6a
AFamhcankou <A.Famchankou@itechart-group.com>
2020-02-25 17:48:45 +0300

    chore: update env vars

--- programming-questionnaire-server/src/db-models/index.js
+++ programming-questionnaire-server/src/db-models/index.js
@@ -1,13 +1,18 @@
 import Sequelize from 'sequelize';
+const sequelize;
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    dialect: 'postgres',
-  },
-);
+if (process.env.DATABASE_CONNECTION_STRING) {
+  sequelize = new Sequelize(process.env.DATABASE_CONNECTION_STRING);
+} else {
+  sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: 'postgres',
+    },
+  );
+}
 
 const models = {
   User: sequelize.import('./user'),
